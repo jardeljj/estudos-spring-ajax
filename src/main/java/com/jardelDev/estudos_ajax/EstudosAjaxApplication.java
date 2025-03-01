84ce55dc8207f17d69992da853f9e4922cbe32a4
Jardel Junior <55859324+jardeljj@users.noreply.github.com>
2025-03-01 17:39:37 -0300

    first commit

---
+++ src/main/java/com/jardelDev/estudos_ajax/EstudosAjaxApplication.java
@@ -0,0 +1,13 @@
+package com.jardelDev.estudos_ajax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudosAjaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudosAjaxApplication.class, args);
+	}
+
+}
