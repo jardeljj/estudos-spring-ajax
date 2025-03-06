2c3e71db5cec1f37b3383d8a25100f28292b6941
Jardel Junior <55859324+jardeljj@users.noreply.github.com>
2025-03-06 00:43:04 -0300

    criando controller para promocao e ajustando o carregamento da home

---
+++ src/main/java/com/jardelDev/estudos_ajax/web/controller/PromocaoController.java
@@ -0,0 +1,32 @@
+package com.jardelDev.estudos_ajax.web.controller;
+
+import com.jardelDev.estudos_ajax.domain.Categoria;
+import com.jardelDev.estudos_ajax.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/promocao")
+public class PromocaoController {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @ModelAttribute("categorias")
+    private List<Categoria> getCategorias(){
+        return categoriaRepository.findAll();
+    }
+
+
+    @GetMapping("/add")
+    public String abrirCadastro(){
+
+        return "promo-add";
+    }
+
+}
