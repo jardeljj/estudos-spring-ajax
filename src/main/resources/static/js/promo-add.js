85c36825a08e5bcd72e7f92124caf28066627b93
Jardel Junior <55859324+jardeljj@users.noreply.github.com>
2025-03-06 16:56:21 -0300

    implementando imagemd e loader para o carregamento do metodo save

--- src/main/resources/static/js/promo-add.js
+++ src/main/resources/static/js/promo-add.js
@@ -18,6 +18,10 @@ $("#form-add-promo").submit(function(evt){
         method: "POST",
         url: "/promocao/save",
         data: promo,
+        beforeSend: function(){
+            $("#form-add-promo").hide();
+            $("#loader-form").addClass("loader").show();
+        },
         success: function() {
         			$("#form-add-promo").each(function() {
         				this.reset();
@@ -29,6 +33,12 @@ $("#form-add-promo").submit(function(evt){
         error: function(xhr){
             console.log("> error: ", xhr.responseText);
             $("#alert").addClass("alert alert-danger").text("Não foi possivel salvar esta promocao");
+        },
+        complete:function(){
+            $("#loader-form").fadeOut(800, function(){
+                                $("#form-add-promo").fadeIn(250);
+                                $("#form-add-promo").removeClass("loader");
+                            });
         }
     });
 
@@ -71,6 +81,5 @@ $("#linkPromocao").on('change', function() {
                 $("#loader-img").removeClass("loader");
             }
        });
-
     }
 });
