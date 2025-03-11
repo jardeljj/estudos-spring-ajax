90de40e4c7ac210fb9adcbcd67b01c9e65c538d9
Jardel Junior <55859324+jardeljj@users.noreply.github.com>
2025-03-11 12:11:20 -0300

    continuando a implantação do metodo para likes com ajax

--- src/main/resources/static/js/promo-list.js
+++ src/main/resources/static/js/promo-list.js
@@ -12,7 +12,7 @@ $(window).scroll(function() {
 	var scrollTop = $(this).scrollTop();
 	var conteudo = $(document).height() - $(window).height();
 	
-	console.log('scrollTop: ', scrollTop, ' | ', 'conteudo:', conteudo);
+	//console.log('scrollTop: ', scrollTop, ' | ', 'conteudo:', conteudo);
 	if (scrollTop >= conteudo) {
     		pageNumber++;
     		setTimeout(function(){
@@ -34,7 +34,7 @@ function loadByScrollBar(pageNumber){
         },
 		success: function( response ) {
 			//console.log("resposta > ", response);
-			console.log("lista > ", response.length);
+			//console.log("lista > ", response.length);
 			if(response.length > 150){
                 $(".row").fadeIn(250, function(){
                     $(this).append(response);
@@ -55,7 +55,19 @@ function loadByScrollBar(pageNumber){
 
 // adicionar likes curtidas
 
-$("button[id*='likes-btn-']").on("click", function(){
-  var id = $(this).attr("id").split("-")[2];
-  console.log("id: ", id);
+$(document).on("click", "button[id*='likes-btn-']",function(){
+    var id = $(this).attr("id").split("-")[2];
+    console.log("id: ", id);
+
+    $.ajax({
+        method: "POST",
+        url: "/promocao/like/" + id,
+        success:  function(response){
+            $("#likes-count-" + id).text(response);
+        },
+        error: function(xhr){
+            alert("Ops, ocorreu um erro: " + xhr.status + ", " + xhr.statusText);
+        },
+    });
+
 });
