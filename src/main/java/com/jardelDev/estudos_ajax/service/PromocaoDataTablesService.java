0560d7b003d41c9f41e5fbca9cea8dcc2197e865
Jardel Junior <55859324+jardeljj@users.noreply.github.com>
2025-03-16 00:25:07 -0300

    filtrando por preço e formatando o preço adicionando 'R$'

--- src/main/java/com/jardelDev/estudos_ajax/service/PromocaoDataTablesService.java
+++ src/main/java/com/jardelDev/estudos_ajax/service/PromocaoDataTablesService.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
+import java.math.BigDecimal;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -47,6 +48,10 @@ public class PromocaoDataTablesService {
     private Page<Promocao> queryBy(String search, PromocaoRepository repository, Pageable pageable) {
         if (search.isEmpty()){
             return repository.findAll(pageable);
+        }
+        if (search.matches("^[0-9]+([.,][0-9]{2})?$")){
+            search = search.replace(",",".");
+            return repository.findByPreco(new BigDecimal(search), pageable);
         }
         return repository.findByTituloOrSiteOrCategoria(search, pageable);
     }
